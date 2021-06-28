c2501d65f7a35347d4d290d86d5c03d13e96efbf
sugitlab <sgmt.snj@gmail.com>
2021-06-29 02:30:43 +0900

    make new task and save it

--- statefulwidget/lib/components/todo_list.dart
+++ statefulwidget/lib/components/todo_list.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import '../models/task.dart';
 
 class TodoList extends StatelessWidget {
-  const TodoList({Key? key}) : super(key: key);
+  const TodoList({Key? key, required this.tasks}) : super(key: key);
+  final List<Task> tasks;
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 10,
+      itemCount: tasks.length,
       itemBuilder: (BuildContext context, int index) {
         return ListTile(
           tileColor: Colors.cyan.withOpacity(0.1 * index),
           title: Center(
-            child: Text('$index'),
+            child: Text('$index: ${tasks.elementAt(index).title}'),
           ),
         );
       },
--- statefulwidget/lib/todo_app.dart
+++ statefulwidget/lib/todo_app.dart
@@ -22,25 +22,90 @@ class TodoAppBody extends StatefulWidget {
 }
 
 class _TodoAppBodyState extends State<TodoAppBody> {
-  final List<Task> tasks = [];
+  List<Task> tasks = [];
 
   void addNewTask(String title) {
     setState(() {
-      tasks.add(Task(title, false));
+      if (tasks.isNotEmpty) {
+        tasks.add(Task(title, false));
+      } else {
+        tasks = [Task(title, false)];
+      }
     });
+  }
+
+  void newTaskBottomSheetForm(BuildContext context, Function(String) callback) {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10),
+          topRight: Radius.circular(10),
+        ),
+      ),
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.only(
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: _builder(context),
+          ),
+        );
+      },
+    );
+  }
+
+  String inputField = '';
+  void updateInput(String text) {
+    setState(() {
+      inputField = text;
+    });
+  }
+
+  Widget _builder(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: TextField(
+            onChanged: (text) {
+              updateInput(text);
+            },
+            autofocus: true,
+            decoration: const InputDecoration(
+              border: UnderlineInputBorder(),
+              hintText: 'New Item',
+              floatingLabelBehavior: FloatingLabelBehavior.never,
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.centerRight,
+          child: TextButton(
+            child: const Text('Save'),
+            onPressed: () {
+              addNewTask(inputField);
+              updateInput('');
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Center(
-        child: TodoList(),
+      body: Center(
+        child: TodoList(tasks: tasks),
       ),
       floatingActionButtonLocation:
           FloatingActionButtonLocation.miniCenterDocked,
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
-        onPressed: () => newTaskBottomSheetForm(context),
+        onPressed: () => newTaskBottomSheetForm(context, addNewTask),
       ),
       bottomNavigationBar: BottomAppBar(
         elevation: 10,
@@ -65,89 +130,6 @@ class _TodoAppBodyState extends State<TodoAppBody> {
           ],
         ),
       ),
-    );
-  }
-}
-
-void newTaskBottomSheetForm(context) {
-  showModalBottomSheet(
-    isScrollControlled: true,
-    context: context,
-    shape: const RoundedRectangleBorder(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(10),
-        topRight: Radius.circular(10),
-      ),
-    ),
-    builder: (BuildContext context) {
-      return SafeArea(
-        child: SingleChildScrollView(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: const NewTaskForm(),
-        ),
-      );
-    },
-  );
-}
-
-class NewTaskForm extends StatefulWidget {
-  const NewTaskForm({Key? key}) : super(key: key);
-  @override
-  _NewTaskFormState createState() => _NewTaskFormState();
-}
-
-class _NewTaskFormState extends State<NewTaskForm> {
-  String inputField = '';
-  void updateInput(String text) {
-    setState(() {
-      inputField = text;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            onChanged: (text) {
-              updateInput(text);
-            },
-            autofocus: true,
-            decoration: const InputDecoration(
-              border: UnderlineInputBorder(),
-              hintText: 'New Item',
-              floatingLabelBehavior: FloatingLabelBehavior.never,
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.centerRight,
-          child: SaveInput(
-            input: inputField,
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class SaveInput extends StatelessWidget {
-  const SaveInput({
-    Key? key,
-    required this.input,
-  }) : super(key: key);
-  final String input;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      child: const Text('Save'),
-      onPressed: () {
-        Navigator.pop(context);
-      },
     );
   }
 }
